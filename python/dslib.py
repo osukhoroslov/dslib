c1bbb3239217e8c0edb886cb7ea1fa17f38a50f5
Oleg Sukhoroslov <oleg.sukhoroslov@gmail.com>
2022-09-02 11:35:46 +0300

    replaced timer id with timer name

--- python/dslib.py
+++ python/dslib.py
@@ -42,19 +42,19 @@ class Context(object):
     def send_local(self, msg: Message):
         self._sent_local_messages.append((msg.type, json.dumps(msg._data)))
 
-    def set_timer(self, timer_id: str, delay: float):
-        if not isinstance(timer_id, str):
-            raise TypeError('timer_id argument has to be str, not {}'.format(type(timer_id)))
+    def set_timer(self, timer_name: str, delay: float):
+        if not isinstance(timer_name, str):
+            raise TypeError('timer_name argument has to be str, not {}'.format(type(timer_name)))
         if not isinstance(delay, (int, float)):
             raise TypeError('delay argument has to be int or float, not {}'.format(type(delay)))
         if delay < 0:
             raise ValueError('delay argument has to be non-negative')
-        self._timer_actions.append((timer_id, delay))
+        self._timer_actions.append((timer_name, delay))
     
-    def cancel_timer(self, timer_id: str):
-        if not isinstance(timer_id, str):
-            raise TypeError('timer_id argument has to be str, not {}'.format(type(timer_id)))
-        self._timer_actions.append((timer_id, -1))
+    def cancel_timer(self, timer_name: str):
+        if not isinstance(timer_name, str):
+            raise TypeError('timer_name argument has to be str, not {}'.format(type(timer_name)))
+        self._timer_actions.append((timer_name, -1))
 
     def time(self) -> float:
         return self._time
@@ -63,12 +63,18 @@ class Context(object):
 class Node:
     @abc.abstractmethod
     def on_local_message(self, msg: Message, ctx: Context):
-        pass
+        """
+        This method is called when a _local_ message is received.
+        """
 
     @abc.abstractmethod
     def on_message(self, msg: Message, sender: str, ctx: Context):
-        pass
+        """
+        This method is called when a message is received.
+        """
 
     @abc.abstractmethod
-    def on_timer(self, timer_id: str, ctx: Context):
-        pass
+    def on_timer(self, timer_name: str, ctx: Context):
+        """
+        This method is called when a timer fires.
+        """
