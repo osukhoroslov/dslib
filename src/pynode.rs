6764e63925915b142aa4adea198da39f3d437712
Oleg Sukhoroslov <oleg.sukhoroslov@gmail.com>
2022-09-03 23:21:51 +0300

    improved python get_size

--- src/pynode.rs
+++ src/pynode.rs
@@ -232,6 +232,8 @@ def get_size(obj, seen=None):
         size += sum([get_size(k, seen) for k in obj.keys()])
     elif hasattr(obj, '__dict__'):
         size += get_size(obj.__dict__, seen)
+    elif hasattr(obj, '__slots__'):
+        size += sum([get_size(getattr(obj, slot), seen) for slot in obj.__slots__])
     elif hasattr(obj, '__iter__') and not isinstance(obj, (str, bytes, bytearray)):
         size += sum([get_size(i, seen) for i in obj])
     return size",
